7a2c4fdf4d314024196b66de0713920340ffd0b5
Thiago Brezinski <thiagobrez@MacBook-Pro-de-Thiago.local>
2021-05-24 20:51:56 -0300

    fix: fix parser

--- index.js
+++ index.js
@@ -16,42 +16,56 @@ if (typeof filePath === 'undefined') {
     process.exit(1);
 }
 
-const messages = fs.readFileSync(filePath).toString().split("\n");
+const text = fs.readFileSync(filePath).toString();
+const dateRegex = /[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]/g;
+
+const dateMatches = text.matchAll(dateRegex);
+const dates = [];
+
+for (const match of dateMatches) {
+    const date = match[0];
+    dates.push(date);
+}
+
+const messages = text.split(dateRegex);
+messages.shift();
 
 function getPosition(string, subString, index) {
     return string.split(subString, index).join(subString).length;
 }
 
-const formattedMessages = messages.map(string => {
+const formattedMessages = messages.map((string, index) => {
     const firstSpaceIndex = getPosition(string, ' ', 1);
     const secondSpaceIndex = getPosition(string, ' ', 2);
-    const plusSignIndex = getPosition(string, '+', 1);
+    const firstHiphenIndex = getPosition(string, '-', 1);
     const secondColonIndex = getPosition(string, ':', 2)
 
-    const date = string.substring(0, firstSpaceIndex) // from start to first space
-    const time = string.substring(firstSpaceIndex + 1, secondSpaceIndex) // from first space to second space
-    const phone = string.substring(plusSignIndex, secondColonIndex) // from `+` to second `:`
-    const message = string.substring(secondColonIndex + 2, string.length) // from second `:` plus 2 chars, until the end
+    const date = dates[index];
+    const time = string.substring(firstSpaceIndex + 1, secondSpaceIndex)
+    const user = string.substring(firstHiphenIndex + 2, secondColonIndex)
+    const message = string.substring(secondColonIndex + 2, string.length)
 
     return {
         date,
         time,
-        phone,
+        user,
         message,
     }
 })
 
+const filteredMessages = formattedMessages.filter(message => !message.user.includes('saiu'))
+
 const fields = [
     {label: 'Data', value: 'date'},
     {label: 'Hora', value: 'time'},
-    {label: 'Telefone', value: 'phone'},
+    {label: 'Usuario', value: 'user'},
     {label: 'Mensagem', value: 'message'},
 ]
 
 const json2csv = new Parser({fields: fields})
 
 try {
-    const csv = json2csv.parse(formattedMessages)
+    const csv = json2csv.parse(filteredMessages)
 
     fs.writeFile('output.csv', csv, function (err) {
         if (err) return console.log('Error writing file: ', err);
